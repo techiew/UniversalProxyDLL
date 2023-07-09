a1e604b9b8f2ab5ecfbd5da892f6afa179ffa878
techiew <marius.tw@hotmail.no>
2023-07-09 15:05:53 +0200

    Clean up DllMain

--- dllmain.cpp
+++ dllmain.cpp
@@ -3,14 +3,6 @@
 
 #include "UniversalProxyDLL.h"
 
-typedef HRESULT(*FpDirectInput8Create)(HINSTANCE, DWORD, REFIID, LPVOID*, LPUNKNOWN);
-void* fpDirectInput8Create = nullptr;
-HRESULT CallbackDirectInput8Create(HINSTANCE hinst, DWORD dwVersion, REFIID riidltf, LPVOID* ppvOut, LPUNKNOWN punkOuter)
-{
-	printf("Callback called!\n");
-	return (*(FpDirectInput8Create*)fpDirectInput8Create)(hinst, dwVersion, riidltf, ppvOut, punkOuter);
-}
-
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
 	if (fdwReason == DLL_PROCESS_ATTACH)
@@ -19,7 +11,6 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 		try
 		{
 			UPD::OpenDebugTerminal();
-			fpDirectInput8Create = UPD::RegisterCallback("DirectInput8Create", &CallbackDirectInput8Create);
 			UPD::CreateProxy(hinstDLL);
 		}
 		catch (std::runtime_error e)
